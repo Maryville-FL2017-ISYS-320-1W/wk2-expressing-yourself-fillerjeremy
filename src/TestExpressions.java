279095976762a93e3498df73214f7904532c9b75
fillerjeremy <33054901+fillerjeremy@users.noreply.github.com>
2017-11-05 19:17:05 -0800

    Test Expressions

--- src/TestExpressions.java
+++ src/TestExpressions.java
@@ -1,19 +1,19 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Jeremy Filler
+  	Date: 11/5/17
 */
 
 /*
  	Your predicted values for the expressions
-	1.
-	2.
-	3.
-	4.
-	5.
-	6.
-	7.
-	8.
+	1.8 -correct
+	2.11 -correct
+	3.12 -correct
+	4.1.876 -incorrect. I think because Java rounds as it goes?
+	5.1.68 Wrong. I think Java rounds up for that data type.
+	6.11 -correct
+	7.4+7  incorrect. It converted the 3 and four into text since the data type is a string.
+	8.7+4 uhm. I really don't know how it's calculating this one.
  
  	After testing the expressions in the main method, indicate above if your predictions were correct.
 */
@@ -21,7 +21,7 @@
 public class TestExpressions {
 
 	public static void main(String[] args) {
-		System.out.println("YOUR_TEST_EXPRESSION");
+		System.out.println(3+4+"2+2");
 	}
 
 }
