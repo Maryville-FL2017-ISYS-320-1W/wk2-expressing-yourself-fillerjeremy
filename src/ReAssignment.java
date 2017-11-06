ddc9c29390e900962a241005f8d410a983152378
fillerjeremy <33054901+fillerjeremy@users.noreply.github.com>
2017-11-05 20:04:49 -0800

    ReAssignment

--- src/ReAssignment.java
+++ src/ReAssignment.java
@@ -6,9 +6,9 @@
 
 /*
  	Your predicted values for variables a, b, and c
-	a => 
-	b =>
-	c => 
+	a => 6 - correct
+	b =>9 - correct
+	c => 14 - it came up as 16. I feel like it should be 15. It is using the updated version of variable A but not the updated version of variable B. WHY??
  
  	After testing the statements in the main method, indicate above if your predictions were correct.
 */
@@ -17,7 +17,14 @@ public class ReAssignment {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 5;
+		int b = 10;
+		int c = b;
+		a = a +1;
+		b = b - 1;
+		c = c + a;
+		b = c%a;
+	System.out.println(b);
 	}
 
 }
