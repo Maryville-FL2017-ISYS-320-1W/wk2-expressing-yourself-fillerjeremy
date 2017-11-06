193a673e3766ca61ddd112a527d8bf127ef007a0
fillerjeremy <33054901+fillerjeremy@users.noreply.github.com>
2017-11-05 19:41:58 -0800

    College student
    I think this is right?

--- src/CollegeStudent.java
+++ src/CollegeStudent.java
@@ -1,13 +1,15 @@
 /*
   	ISYS 320
-  	Name(s):
-  	Date: 
+  	Name(s):Jeremy Filler
+  	Date: 11/5/17
 */
 
 public class CollegeStudent {
 
 	public static void main(String[] args) {
-
+		String student;
+		int studentclasses;
+		double studentgpa;
 	}
 
 }
